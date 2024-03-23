04428034ed9b85dcc49026d6963339619d492e47
levanthieu97 <levanthieu97@gmail.com>
2024-03-24 00:41:54 +0700

    fix styled-icon

--- src/apps/templates/Michi-Ramen/components/Footer/styled.js
+++ src/apps/templates/Michi-Ramen/components/Footer/styled.js
@@ -1,7 +1,7 @@
-import { Row } from "src/apps/components/Row";
-import styled, { css } from "styled-components";
-import {StyledIconBase} from '@styled-icons/styled-icon'
-import { MEDIA, usingMedia } from "src/styles/global.styled";
+import { Row } from 'src/apps/components/Row'
+import styled, { css } from 'styled-components'
+import { StyledIconBase } from '@styled-icons/styled-icon'
+import { MEDIA, usingMedia } from 'src/styles/global.styled'
 
 export const FooterContainer = styled(Row)`
   margin-block: 30px;
@@ -9,26 +9,32 @@ export const FooterContainer = styled(Row)`
   grid-template-columns: 1fr;
   column-gap: 30px;
   row-gap: 15px;
-  ${usingMedia({
-    [MEDIA.TABLET]: css`
-      grid-template-columns: repeat(2, 1fr);
-    `,
-    [MEDIA.DESKTOP]: css`
-      grid-template-columns: repeat(4, 1fr);
-      row-gap: 30px;
-    `,
-  }, 'min-width')}
+  ${usingMedia(
+    {
+      [MEDIA.TABLET]: css`
+        grid-template-columns: repeat(2, 1fr);
+      `,
+      [MEDIA.DESKTOP]: css`
+        grid-template-columns: repeat(4, 1fr);
+        row-gap: 30px;
+      `,
+    },
+    'min-width'
+  )}
 `
 
 export const InfoStyled = styled(Row)`
   color: #f3f3f4;
   font-size: 22px;
   gap: 5px;
-  ${usingMedia({
-    [MEDIA.TABLET]: css`
-      width: 50%;
-    `,
-  }, 'max-width')} 
+  ${usingMedia(
+    {
+      [MEDIA.TABLET]: css`
+        width: 50%;
+      `,
+    },
+    'max-width'
+  )}
 `
 
 export const IconStyled = styled.span`
@@ -51,7 +57,7 @@ export const Typography = styled.p`
   text-align: left;
 `
 
-export const NameStyled  = styled.span`
+export const NameStyled = styled.span`
   color: #ffdb59;
   font-size: 18px;
 `
